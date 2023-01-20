52537d154b6c646ebf10ee0c7f62caf7c4edc269
Cassiano Rocha Dias <116639671+Ness62hz@users.noreply.github.com>
2023-01-19 22:04:26 -0300

    Add files via upload

---
+++ CARREGADOR/script.js
@@ -0,0 +1,15 @@
+let externo = document.querySelector('.externo');
+let interno = document.querySelector('.interno');
+let percent = document.querySelector('span');
+let count = 0;
+interno.addEventListener('click', function(){
+    let loading = setInterval(animate, 200);
+    function animate(){
+        if(count == 100){
+            clearInterval()       
+        }else{
+            count = count+1;
+            percent.textContent= count + '%';
+        }
+    }
+})
